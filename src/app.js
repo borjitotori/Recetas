3f71e4a9779024b388e8b38e6769cdf31ce7d3f9
borjitotori <33838047+borjitotori@users.noreply.github.com>
2019-11-07 15:46:59 +0100

    implemantation of first and second step, and half of third

--- src/app.js
+++ src/app.js
@@ -4,14 +4,17 @@ import request from 'request'
 import * as uuid from 'uuid'
 import date from 'date-and-time';
 
-const authorData = [], ingredientData = [] 
-const recipeData = [];
+const authorData = [], ingredientData = [], recipeData = [];
+
+authorData.push({id: "123", name: "Borja", email: "awesad@diaw"},{id: "342", name: "Agustin", email: "dwadf@fnans"})
+ingredientData.push({id: "122", name: "Guatemala"}, {id: "675", name: "Guatepeor"})
+recipeData.push({id: "322", title: "Something good", description: "NONE", date: 123, author: "342", ingredients:["122", "675"]})
 
 const typeDefs=`
     type Query {
         authors: [Author!]
         recipes: [Recipe!]
-        ingredient: [Ingredient!]
+        ingredients: [Ingredient!]
 		authorRecipes (id: ID!): [Recipe!]
 		ingredientRecipes (id: ID!): [Recipe!]
     }
@@ -31,13 +34,19 @@ const typeDefs=`
     }
     type Ingredient{
         id: ID!
-		name: Author!
+		name: String!
 		recipes: [Recipe!]
     }
     type Mutation{
         addAuthor(name: String!, email: String!): Author!
-        addRecipe(title: String!, description: String!, author: ID!, ingredients: ID!): Recipe!
-		addIngredient(name: String!): Ingredient!
+        addRecipe(title: String!, description: String!, author: ID!, ingredients: [ID!]!): Recipe!
+        addIngredient(name: String!): Ingredient!
+        deleteRecipe(id: ID!): String!
+        deleteAuthor(id: ID!): String!
+        deleteIngredient(id: ID!): String!
+        actualizeAuthor(id: ID!, name: String, email: String): String!
+        actualizeRecipe(id: ID!, title: String, description: String, addingredient: ID, delingredient: ID): String!
+        actualizeIngredient(id: ID!, name: String): String!
     }
 `
 
@@ -54,19 +63,18 @@ const resolvers = {
             const authorID = parent.author;
             const result = authorData.find(obj => obj.id === authorID);
             return result;
-        }
+        },
 		ingredients:(parent, args, ctx, info) => {
-			const ingredientID = parent.ingredient;
-			const result = [];
-			const result = ingredientData.filter(obj => obj.id === ingredientID);
+			const ingredientID = parent.ingredients;
+			const result = ingredientData.filter(obj => ingredientID.includes(obj.id));
 			return result;
 		}
     },
 	
 	Ingredient: {
-		recipe:(parent, args, ctx, info) => {
+		recipes:(parent, args, ctx, info) => {
 			const ingredientID = parent.id;
-			return recipeData.filter(obj => obj.ingredient === ingredientID);
+			return recipeData.filter(obj => obj.ingredients.includes(ingredientID));
 		}
 	},
 
@@ -88,12 +96,12 @@ const resolvers = {
                 throw new Error (`No recipe by user ${args.id}`);
             }
 			
-            return recipeData.find(obj=>obj.author === args.id);
+            return recipeData.filter(obj=>obj.author === args.id);
         },
 		
-	ingredientRecipes: (parent, args, ctx, info) => {
-            if(!recipeData.some(obj=>obj.ingredient === args.id)){
-                throw new Error (`No recipe with ingredient ${args.ingredient}`);
+	    ingredientRecipes: (parent, args, ctx, info) => {
+            if(!recipeData.some(obj=>obj.ingredients.includes(args.id))){
+                throw new Error (`No recipe with ingredient ${args.ingredients}`);
             }
             
             return recipeData.filter(obj => obj.ingredients.some(obj => obj === args.id));;
@@ -128,7 +136,7 @@ const resolvers = {
                 description,
                 id: uuid.v4(),
                 author,
-		ingredients,
+		        ingredients,
                 date: new Date().getDate()
             }
 
@@ -136,7 +144,7 @@ const resolvers = {
             return recipe;
         },
 		
-	addIngredient: (parent, args, ctx, info) => {
+	    addIngredient: (parent, args, ctx, info) => {
             const {name} = args;
             if (ingredientData.some(obj=>obj.name === name)){
                 throw new Error(`Ingredient with name ${name} already exist`);
@@ -150,7 +158,74 @@ const resolvers = {
             ingredientData.push(ingredient);
             return ingredient;
         },
+
+        deleteRecipe: (parent, args, ctx, info) =>{
+            const message = (`git gud recipe with id ${args.id}`)
+            recipeData.splice(recipeData.findIndex(obj => obj.id === args.id), 1)
+            return message
+        },
+
+        deleteAuthor: (parent, args, ctx, info) =>{
+            const message = (`git gud user with id ${args.id}`)
+            authorData.splice(authorData.findIndex(obj => obj.id === args.id), 1)
+            if (recipeData.some(obj=>obj.author === args.id)){
+                recipeData.splice(recipeData.findIndex(obj => obj.author === args.id), 1)
+            }
+            return message
+        },
+
+        deleteIngredient: (parent, args, ctx, info) =>{
+            const message = (`git gud ingredient with id ${args.id}`)
+            ingredientData.splice(ingredientData.findIndex(obj => obj.id === args.id), 1)
+            if (recipeData.some(obj=>obj.ingredients.includes(args.id))){author
+                recipeData.splice(recipeData.findIndex(obj => obj.author === args.id), 1)
+            }
+            return message
+        },
+
+        actualizeAuthor: (parent, args, ctx, info) =>{
+            const message = ("GIT GUD")
+            const {id, name, email} = args;
+            if(name){
+                authorData.find(obj => obj.id === id).name=name
+            }
+            if(email){
+                authorData.find(obj => obj.id === id).email=email
+            }
+            return message
+        },
+
+        actualizeRecipe: (parent, args, ctx, info) =>{
+            const message = ("GIT GUD")
+            const {id, name} = args;
+            if(name){
+                ingredientData.find(obj => obj.id === id).name=name
+            }
+            return message
+        },
+
+        actualizeRecipe: (parent, args, ctx, info) =>{
+
+            const message = ("GIT GUD")
+            const {id, title, description, addingredient, delingredient} = args;
+            const index = recipeData.findIndex(obj => obj.id === id)
+            if(title){
+                recipeData.find(obj => obj.id === id).title=title
+            }
+            if(description){
+                recipeData.find(obj => obj.id === id).description=description
+            }
+            if(addingredient){
+                recipeData.find(obj => obj.id === id).ingredients.push(ingredientData.find(obj => obj.id === addingredient));
+            }
+            if(delingredient){
+                recipeData.find(obj => obj.id === id).ingredients.splice(recipeData.findIndex(obj => obj.ingredients === delingredient), 1)
+            }
+            return message
+        }
     }
 }
+
+
 const server = new GraphQLServer({typeDefs, resolvers});
 server.start(()=> console.log("Server started"));
